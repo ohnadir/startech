23ef7e4e620ce36925555882da3b78c7d5927caf
Nadir Ahmed <qx.nadir@gmail.com>
2022-09-21 19:50:02 +0600

    implement categories section

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,14 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Navbar from './Component/Navbar';
 import SearchResult from './Component/SearchResult';
+import Home from './Page/Home';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
-      <h1>Nadir Ahmed</h1>
       <Routes>
+        <Route path="/" element={<Home/>} />
         <Route path="/search/:keyword" element={<SearchResult/>} />
       </Routes>
     </div>
---
+++ frontend/src/Page/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Category from '../Component/Category';
+import Marquee from '../Component/Marquee';
+
+const Home =()=>{
+    return (
+      <div className='bg-[#f2f4f8]'>
+        <Marquee/>
+        <Category/>
+        
+      </div>
+    )
+}
+
+export default Home;
