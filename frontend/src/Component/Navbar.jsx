5d5d308c4514c47700d16edf353182c8f2518d0b
Nadir Ahmed <qx.nadir@gmail.com>
2022-09-21 14:43:54 +0600

    implement navbar

---
+++ frontend/src/Component/Navbar.jsx
@@ -0,0 +1,66 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { CgProfile } from 'react-icons/cg';
+import { BsCartCheckFill } from 'react-icons/bs';
+import { BiSearchAlt2 } from 'react-icons/bi';
+import '../Style/Navbar.css'
+import Layout from './Layout';
+
+const Navbar = (props) => {
+    const navigate = useNavigate();
+    const menuLists = <>
+        <div className="flex relative">
+            <span onClick={()=>navigate('/cart')} ><BsCartCheckFill className='cursor-pointer text-2xl' /></span>
+            <span className="absolute top-[-11px] left-[13px] bg-[#669900]  rounded-full w-[20px] h-[20px] flex items-center justify-center p-[3px]">1</span>
+        </div>
+        <div>
+            <div className="dropdown">
+                <CgProfile className='dropBtn' />
+                <div className="dropdown-content">
+                    <ul>
+                        <li>Profile</li>
+                        <li>Profile</li>
+                        <li>Profile</li>
+                    </ul>
+                    
+                        {/* {
+                            user && <Link to='/dashboard' className='cursor-pointer hover:bg-gray-200 p-2'>
+                                <label htmlFor="dashboardDrawer" className=" drawer-button">Dashboard</label>
+                            </Link>
+                        }
+                        {
+                            user ?
+                            <button onClick={handleSignOut} className='cursor-pointer hover:bg-gray-200 p-2'>Sign out</button>
+                            : 
+                            <Link to='/login' className='cursor-pointer hover:bg-gray-200 p-2'>Login</Link>
+                        } */}
+                </div>
+            </div>
+        </div> 
+    </>
+    return (
+        <Layout>
+            <div className='bg-white border'>
+                <div className='max-w-7xl mx-auto px-2 '>
+                    <div className=' flex items-center h-14 justify-between text-black  relative z-50'>
+                        <div className=''>
+                            <span className='cursor-pointer' onClick={()=>navigate('/home')}>StarTech</span>
+                        </div>
+                        <div className='searchContainer'>
+                            <div className='flex items-center '>
+                                <input className='searchInput' type="text" />
+                                <button className='searchBtn'><BiSearchAlt2/></button>
+                            </div>
+                        </div>
+                        <div className='flex gap-5 items-center'>
+                            {menuLists}
+                        </div>
+                        
+                    </div>
+                </div>
+            </div>
+        </Layout>
+    );
+};
+
+export default Navbar;
