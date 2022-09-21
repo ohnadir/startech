13af400df36d297361a37196ca8d5a8dcb7d2c11
Nadir Ahmed <qx.nadir@gmail.com>
2022-09-21 19:51:20 +0600

    marquee section add

---
+++ frontend/src/Component/Marquee.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Marquee =()=>{
+    return (
+      <div className='pt-[35px] max-w-7xl mx-auto px-2 '>
+        <div className='bg-white rounded-xl px-3  py-[6px]'>
+            <marquee>22nd September Thursday, our all branch as well as online activities are open and operational.</marquee>
+        </div>
+      </div>
+    )
+}
+
+export default Marquee;
