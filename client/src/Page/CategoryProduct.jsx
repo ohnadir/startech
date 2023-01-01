469d870a677152f366c032b21df2882e5f13d630
Nadir Hossain <nadirhossain336@gmail.com>
2023-01-01 21:29:13 +0600

    filter implement for CategoryProduct

--- client/src/Page/CategoryProduct.jsx
+++ client/src/Page/CategoryProduct.jsx
@@ -1,18 +1,27 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
 import { HiHome } from 'react-icons/hi';
-import { BsFilter } from 'react-icons/bs';
 import { FiFilter } from 'react-icons/fi';
-import { MdOutlineKeyboardArrowDown } from 'react-icons/md';
+import { MdOutlineKeyboardArrowDown, MdOutlineKeyboardArrowUp } from 'react-icons/md';
 import { Link, useParams, useNavigate } from 'react-router-dom';
 import "../Style/CategoryProduct.css";
-
+import { Drawer } from 'antd';
+import { Slider } from 'antd';
 
 const CategoryProduct = () => {
     const { keyword } = useParams();
     const [searchProduct, setSearchProduct] = useState([]);
-    
+    const [open, setOpen] = useState(false);
+    const [sort, setSort] = useState(false);
+    const [sortCard, setSortCard] = useState(false)
+    const [value, setValue] = useState([0, 100]);
+    const [collapse, setCollapse] = useState(true)
     const navigate = useNavigate();
+
+    const onAfterChange = (value) => {
+        setValue(value);
+    };
+
     useEffect(() => {
         // axios.get(`https://startech-server.vercel.app/search?q=${keyword}`)
         // axios.get(`/products/search?q=${keyword}`)
@@ -45,17 +54,79 @@ const CategoryProduct = () => {
                     </div>
                     <div className='flex items-center gap-2'>
                         <p className='m-0'>Sort By:</p>
-                        <div>
-                            <div className='sortContainer'>
+                        <div className='relative'>
+                            <div className='sortContainer' onClick={()=>setSortCard(!sortCard)}>
                                 <p className='m-0 text-[13px] font-semibold'>Default</p>
                                 <MdOutlineKeyboardArrowDown className='text-[20px] ml-5'/>
                             </div>
+                            {
+                               sortCard &&
+                                <div className='sortCard'>
+                                    <ul className='m-0'>
+                                        <li>Default</li>
+                                        <li>Price (Low > High)</li>
+                                        <li>Price (Hight > Low)</li>
+                                    </ul>
+                                </div>
+                            }
+                            
                         </div>
                     </div>
                 </div>
             </section>
-            <section className='product'>
-                <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-5'>
+            <section className='product flex gap-5'>
+                <div className='w-[20%] m-0 p-0 hidden lg:block'>
+                    <div className='drawer p-[10px] lg:p-0'>
+                        <div className="rangeContainer">
+                            <h1 className='m-0 px-[15px] pt-[15px] text-lg font-semibold'>Price Range</h1>
+                            <div className='filterDivider'></div>
+                            <div className='p-5'>
+                                <Slider
+                                    range
+                                    step={1}
+                                    tooltip="false"
+                                    defaultValue={[0, 100]}
+                                    onAfterChange={onAfterChange}
+                                />
+                                <div className='flex justify-between items-center pt-5'>
+                                    <p className='m-0 border px-5'>{value[0]}</p>
+                                    <p className='m-0 border px-5'>{value[1]}</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className='availability mt-3'>
+                            <div className='flex justify-between items-center pt-[15px] px-[15px]'>
+                                <h1 className='m-0    font-semibold'>Availability</h1>
+                                {
+                                        collapse ?
+                                            <MdOutlineKeyboardArrowUp onClick={()=>setCollapse(!collapse)} className='text-[23px] cursor-pointer' /> 
+                                        :
+                                        <MdOutlineKeyboardArrowDown onClick={()=>setCollapse(!collapse)} className='text-[23px] cursor-pointer' />
+
+                                    }
+                            </div>
+                            <div className='filterDivider'></div>
+                            {
+                                collapse &&
+                                <div className='p-[15px] grid grid-cols-1 gap-2'>
+                                    <div className='flex items-center gap-2'>
+                                        <input type="checkbox" name="stock" id="" />
+                                        <label className='text-black' htmlFor="">In Stock</label>
+                                    </div>
+                                    <div className='flex items-center gap-2'>
+                                        <input type="checkbox" name="preOrder" id="" />
+                                        <label className='text-black' htmlFor="">Pre Order</label>
+                                    </div>
+                                    <div  className='flex items-center gap-2'>
+                                        <input type="checkbox" name="upComing" id="" />
+                                        <label className='text-black' htmlFor="">Up Coming</label>
+                                    </div>
+                                </div>
+                            }
+                        </div>
+                        </div>
+                    </div>
+                <div className='grid grid-cols-1 w-full lg:w-[80%] sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-5'>
                     {
                         searchProduct?.map((product)=>
                             <Link to={`/productDetail/${product._id}` }>
@@ -73,9 +144,70 @@ const CategoryProduct = () => {
                         )
                     }
                 </div>
+                
             </section>
             
-            
+            <section className='drawer'>
+                <Drawer
+                    className='lg:hidden' 
+                    bodyStyle={{"padding": "0px"}}
+                    headerStyle={{"borderBottom": "0px ", "display": "none"}}
+                    placement="right" closeIcon={false} 
+                    onClose={()=> setOpen(false)}
+                    open={open}>
+                        <div className='drawer'>
+                            <div className="rangeContainer">
+                                <h1 className='m-0 px-[15px] pt-[15px] text-lg font-semibold'>Price Range</h1>
+                                <div className='filterDivider'></div>
+                                <div className='p-5'>
+                                    <Slider
+                                        range
+                                        step={1}
+                                        tooltip="false"
+                                        defaultValue={[0, 100]}
+                                        onAfterChange={onAfterChange}
+                                    />
+                                    <div className='flex justify-between items-center pt-5'>
+                                        <p className='m-0 border px-5'>{value[0]}</p>
+                                        <p className='m-0 border px-5'>{value[1]}</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className='availability mt-3'>
+                                <div className='flex justify-between items-center pt-[15px] px-[15px]'>
+                                    <h1 className='m-0    font-semibold'>Availability</h1>
+                                    {
+                                        collapse ?
+                                            <MdOutlineKeyboardArrowUp onClick={()=>setCollapse(!collapse)} className='text-[23px] cursor-pointer' /> 
+                                        :
+                                        <MdOutlineKeyboardArrowDown onClick={()=>setCollapse(!collapse)} className='text-[23px] cursor-pointer' />
+
+                                    }
+                                </div>
+                                <div className='filterDivider'></div>
+                                {
+                                    collapse &&
+                                    <div className='p-[15px] grid grid-cols-1 gap-2'>
+                                        <div className='flex items-center gap-2'>
+                                            <input type="checkbox" name="stock" id="" />
+                                            <label className='text-black' htmlFor="">In Stock</label>
+                                        </div>
+                                        <div className='flex items-center gap-2'>
+                                            <input type="checkbox" name="preOrder" id="" />
+                                            <label className='text-black' htmlFor="">Pre Order</label>
+                                        </div>
+                                        <div  className='flex items-center gap-2'>
+                                            <input type="checkbox" name="upComing" id="" />
+                                            <label className='text-black' htmlFor="">Up Coming</label>
+                                        </div>
+                                    </div>
+                                }
+                                
+                            </div>
+                            
+                        </div>
+                </Drawer>
+            </section>
     </div>
     )
 }
