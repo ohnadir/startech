98a87a0d52f332cd96fefa3a439cd1204abc5deb
Nadir Hossain <nadirhossain336@gmail.com>
2023-01-02 16:19:56 +0600

    margin on top of the pagination

--- client/src/Component/Product.jsx
+++ client/src/Component/Product.jsx
@@ -39,7 +39,7 @@ const Product = () => {
                 )
             }
         </div>
-        <section className="pagination">
+        <section className="pagination mt-10">
         {
                     [...Array(pages).keys()].map(number => <button
                         key={number}
