cc82db4a09942fcbe2070063546e948b99782dd1
Nadir Hossain <nadirhossain336@gmail.com>
2023-01-03 10:11:46 +0600

    spiner implement

--- client/src/Component/Product.jsx
+++ client/src/Component/Product.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
 import '../Style/Product.css';
+import { Spin } from 'antd';
 
 const Product = () => {
     const [products, setProducts] = useState([]);
@@ -20,42 +21,50 @@ const Product = () => {
     const pages = Math.ceil(count / size);
 
   return (
-    <div className='max-w-7xl mx-auto px-5 py-[40px]'>
-        <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-5'>
-            {
-                products?.map((product)=>
-                    <Link to={`/productDetail/${product._id}`}>
-                        <div key={product.id} className='bg-white shadow-lg w-[300px] md:w-fit mx-auto rounded-[6px]'>
-                            <div className='px-5 pt-5'>
-                                <img src={product?.productPictures[0]} alt="" />
+    <>
+     {
+        products === [] ?    
+            <div className='w-full h-screen flex items-center justify-center'><Spin/></div>
+        :  
+        <div className='max-w-7xl mx-auto px-5 py-[40px]'>
+            <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-5'>
+                {
+                    products?.map((product)=>
+                        <Link to={`/productDetail/${product._id}`}>
+                            <div key={product.id} className='bg-white shadow-lg w-[300px] md:w-fit mx-auto rounded-[6px]'>
+                                <div className='px-5 pt-5'>
+                                    <img src={product?.productPictures[0]} alt="" />
+                                </div>
+                                <div className='divider'></div>
+                                <div className='p-5 flex flex-col justify-between'>
+                                    <h3 className='text-[15px] font-[400]'>{product.name}</h3>
+                                    <p className='text-[#ef4a23] text-[17px] font-[600]'>{product?.price} ৳</p>
+                                </div>
                             </div>
-                            <div className='divider'></div>
-                            <div className='p-5 flex flex-col justify-between'>
-                                <h3 className='text-[15px] font-[400]'>{product.name}</h3>
-                                <p className='text-[#ef4a23] text-[17px] font-[600]'>{product?.price} ৳</p>
-                            </div>
-                        </div>
-                    </Link>
-                )
-            }
-        </div>
-        <section className="pagination mt-10 flex items-center justify-center" >
-            {
-                [...Array(pages).keys()].map(number => <button
-                    key={number}
-                    className={page === number ? 'selected' : ''}
-                    onClick={() => setPage(number)}>
-                    {number + 1}
-                </button>)
-            }
-            <select className='sizeContainer' onChange={event => setSize(event.target.value)}>
-                    <option value="5">5</option>
-                    <option value="10" selected>10</option>
-                    <option value="15">15</option>
-                    <option value="20">20</option>
-                </select>
-        </section>
-    </div>
+                        </Link>
+                    )
+                }
+            </div>
+            <section className="pagination mt-10 flex items-center justify-center" >
+                {
+                    [...Array(pages).keys()].map(number => <button
+                        key={number}
+                        className={page === number ? 'selected' : ''}
+                        onClick={() => setPage(number)}>
+                        {number + 1}
+                    </button>)
+                }
+                <select className='sizeContainer' onChange={event => setSize(event.target.value)}>
+                        <option value="5">5</option>
+                        <option value="10" selected>10</option>
+                        <option value="15">15</option>
+                        <option value="20">20</option>
+                    </select>
+            </section>
+        </div> 
+     }
+    </>
+    
   )
 }
 
--- client/src/Component/SearchResult.jsx
+++ client/src/Component/SearchResult.jsx
@@ -4,6 +4,7 @@ import { useParams } from 'react-router-dom';
 import '../Style/SearchResult.css'
 import { HiHome } from 'react-icons/hi';
 import { Link, useNavigate } from 'react-router-dom';
+import { Spin } from 'antd';
 
 const SearchResult = () => {
     const { keyword } = useParams();
@@ -26,32 +27,39 @@ const SearchResult = () => {
         });
     }, [keyword]);
     return (
-        <div className='max-w-7xl mx-auto  searchResult'>
-            
-            <div className='p-5'>
-                <div className='navigate'>
-                    <HiHome onClick={()=>navigate('/home')} className='text-[#666] cursor-pointer'/> <span>/</span> 
-                    <span className="cursor-pointer">Search</span> 
-                </div>
-                <h1 className='p-4 font-semibold searchHeader m-0 my-5'>Search - {keyword}</h1>
-                <div className='grid grid-flow-col-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5'>
-                    {
-                        searchProduct.map(product =>
-                            <Link to={`/productDetail/${product._id}`}>
-                                <div key={product._id} className="searchProduct">
-                                    <div className='p-4'>
-                                        <img className='w-[150px] mx-auto' src={product?.productPictures[0]} alt="" />
-                                        <h1 className='text-black font-semibold text-[13px] pt-5'>{product?.name}</h1>
-                                        <p className='text-[14px] m-0 pb-2 text-[#666]'>{product?.desc}</p>
-                                        <h1 className='text-[#ef4a23] text-[16px] font-semibold'>{product?.price}৳</h1>
-                                    </div>
-                                </div>
-                            </Link> 
-                        )
-                    }
-                </div>
-            </div>
-        </div>
+        <>
+            {
+                searchProduct === [] ?
+                    <div className='w-full h-screen flex items-center justify-center'><Spin/></div>
+                :
+                    <div className='max-w-7xl mx-auto  searchResult'>
+                        <div className='p-5'>
+                            <div className='navigate'>
+                                <HiHome onClick={()=>navigate('/home')} className='text-[#666] cursor-pointer'/> <span>/</span> 
+                                <span className="cursor-pointer">Search</span> 
+                            </div>
+                            <h1 className='p-4 font-semibold searchHeader m-0 my-5'>Search - {keyword}</h1>
+                            <div className='grid grid-flow-col-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5'>
+                                {
+                                    searchProduct.map(product =>
+                                        <Link to={`/productDetail/${product._id}`}>
+                                            <div key={product._id} className="searchProduct">
+                                                <div className='p-4'>
+                                                    <img className='w-[150px] mx-auto' src={product?.productPictures[0]} alt="" />
+                                                    <h1 className='text-black font-semibold text-[13px] pt-5'>{product?.name}</h1>
+                                                    <p className='text-[14px] m-0 pb-2 text-[#666]'>{product?.desc}</p>
+                                                    <h1 className='text-[#ef4a23] text-[16px] font-semibold'>{product?.price}৳</h1>
+                                                </div>
+                                            </div>
+                                        </Link> 
+                                    )
+                                }
+                            </div>
+                        </div>
+                    </div>
+            }
+        </>
+        
     );
 };
 
