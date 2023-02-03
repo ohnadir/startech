6d13a4a119bd102c1b56f25cfa2a48c972b6d2eb
Nadir Hossain <nadirhossain336@gmail.com>
2023-02-03 18:18:21 +0600

    something change on jwtToken.js

--- server/utils/jwtToken.js
+++ server/utils/jwtToken.js
@@ -6,13 +6,12 @@ const sendToken = (user, statusCode, res) => {
         expires: new Date(
             Date.now() + process.env.COOKIE_EXPIRES_TIME * 24 * 60 * 60 * 1000
         ),
-        httpOnly: true,
-        signed: true
+        httpOnly: true
     }
     res.status(statusCode).cookie('token', token, options).json({
         success: true,
         token,
-        user
+        ...user._doc
     })
 }
 module.exports = sendToken;
