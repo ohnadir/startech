d9c9950b92483b75714db5163d31896965129b3c
Nadir Hossain <nadirhossain336@gmail.com>
2023-02-12 12:15:02 +0600

    fetch all product into the admin products

--- client/src/Page/Dashboard/Products.jsx
+++ client/src/Page/Dashboard/Products.jsx
@@ -1,6 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Table } from 'antd'
+import { useDispatch, useSelector } from 'react-redux'
+import { getProducts } from '../../actions/productActions'
 
+const columns = [
+  {
+    title: 'Name',
+    dataIndex: 'name',
+    key: 'name',
+  },
+  {
+    title: 'Brand',
+    dataIndex: 'brand',
+    key: 'brand',
+  },
+  {
+    title: 'Category',
+    dataIndex: 'category',
+    key: 'category',
+  },
+  {
+    title: 'Price',
+    dataIndex: 'price',
+    key: 'price',
+  },
+];
 const Products = () => {
     const dataSource = [
         {
@@ -16,28 +40,45 @@ const Products = () => {
           address: '10 Downing Street',
         },
     ];
-      
-    const columns = [
-        {
-          title: 'Name',
-          dataIndex: 'name',
-          key: 'name',
-        },
-        {
-          title: 'Age',
-          dataIndex: 'age',
-          key: 'age',
-        },
-        {
-          title: 'Address',
-          dataIndex: 'address',
-          key: 'address',
-        },
-    ];
+    const [counts, setCounts] = useState();
+    const [size, setSize] = useState(8);
+    const [page, setPage] = useState(0);
+    const dispatch = useDispatch();
+    const { products, error, count } = useSelector(state => state?.products);
+    
+    useEffect(() => {
+        dispatch(getProducts(page, size));
+        setCounts(count);
+    }, [ page, size, error]);
+    
+    const pages = Number(Math.ceil(counts / size)) || 4;
+    const arrayPages = [...Array(pages && pages).keys()];
+    
     return (
         <div>
             <h1 className='text-center font-semibold text-xl pb-5'>All Products from Database</h1>
-            <Table dataSource={dataSource} columns={columns} pagination={false} />
+            <Table dataSource={products} columns={columns} pagination={false} />
+            <section className="pagination mt-10 flex items-center justify-center" >
+                        <div>
+
+                            {
+                                arrayPages?.map(number => <button
+                                    key={number}
+                                    className={page === number ? 'selected' : ''}
+                                    onClick={() => setPage(number)}
+                                    >
+                                {number + 1}
+                            </button>)
+                            }
+                        </div>
+                        <select className='sizeContainer' onChange={event => setSize(event.target.value)}>
+                                <option value="">Please choose an option</option>
+                                <option value= "8">8</option>
+                                <option value= "10">10</option>
+                                <option value= "15">15</option>
+                                <option value= "20">20</option>
+                        </select>
+                    </section>
         </div>
     )
 }
