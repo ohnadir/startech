6a9cdc0ae764e1edf7139b4a05a180a1d88a624d
Nadir Hossain <nadirhossain336@gmail.com>
2023-07-30 12:46:19 +0600

    another okay

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ connectDb();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use("*", cors({
+app.use(cors({
   origin: 'http://localhost:3000',
   credentials : true,
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
