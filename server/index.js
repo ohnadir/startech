19742316766c0340c7f5ab7d48159b678e5a31f0
Nadir Hossain <nadirhossain336@gmail.com>
2023-08-02 21:39:43 +0600

    done

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ connectDb();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: '*',
   credentials : true,
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
 }));
